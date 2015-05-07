22d0b239cd87f68e2226ebac143301f4b8757390
Hyeon Kim <simnalamburt@gmail.com>
2015-05-07 11:02:05 +0900

    코드 리팩터링, 테스트 추가함

--- DateCalculator.rb
+++ DateCalculator.rb
@@ -1,35 +1,48 @@
 class DateCalculator
+  attr_accessor :date, :year
 
-  def self.convert(d) # static method. not good!
-    y = 1980
-    while (d > 365) do
-      if (y % 400 == 0 ||
-          (y % 4 == 0 && y % 100 != 0))
-        if (d > 366)
-          d -= 366
-          y += 1
-        end
-      else
-        d -= 365
-        y += 1
-      end
-    end
-    return y
+  def initialize(date)
+    @date = date
+    @year = 1980
   end
 
+  def convert()
+    while leap_year? ? add_leap_year : add_regular_year
+    end
+    @year
+  end
+
+  def leap_year?
+    @year % 400 == 0 || (@year % 4 == 0 && @year % 100 != 0)
+  end
+
+  def add_leap_year
+    return false if @date < 366
+    @date -= 366
+    @year += 1
+    true
+  end
+
+  def add_regular_year
+    return false if @date < 365
+    @date -= 365
+    @year += 1
+    true
+  end
 end
 
 
 if __FILE__ == $0
-    puts DateCalculator.convert(ARGV[0].to_i)
+  year = DateCalculator.new ARGV[0].to_i
+  puts year.convert
 end
 
-# pseudo code
 
-# start with Year = 1980
-# while (days remaining > 365)
-#   if Year is a leap year
-#       then if possible, peel off 366 days and advance Year by 1
-#   else
-#       peel off 365 days and advance Year by 1
-# return Year
+__END__
+start with Year = 1980
+while (days remaining > 365)
+  if Year is a leap year
+    then if possible, peel off 366 days and advance Year by 1
+  else
+    peel off 365 days and advance Year by 1
+return Year
